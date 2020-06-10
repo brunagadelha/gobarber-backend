3906f687412c9b9fd4e6f6b2b31ed68e90323c25
Bruna Gadelha <bruna_gadelha@live.com>
2020-06-10 19:14:48 -0300

    exceptions

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 import uploadConfig from '../config/upload';
 
@@ -18,7 +20,10 @@ export default class UpdateUserAvatarService {
         });
 
         if (!user) {
-            throw new Error('Only authenticated users can change avatar');
+            throw new AppError(
+                'Only authenticated users can change avatar',
+                401,
+            );
         }
 
         if (user.avatar) {
